c72b62c1a103206684f4ba9fd81e5fc8a1e9a94b
dsnesia <109241727+dsnesia@users.noreply.github.com>
2025-06-14 20:28:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "YOUR_BOT_TOKEN",
-  allowedDevelopers: ['YOYR_TOKEN'],
+  BOT_TOKEN: "7849701689:AAGEpNM3BA-o9rrv_KsXCJv-SaKFb_bTtTg",
+  allowedDevelopers: ['7849701689:AAGEpNM3BA-o9rrv_KsXCJv-SaKFb_bTtTg'],
 };
 global.idch = "120363417446780608@newsletter"
